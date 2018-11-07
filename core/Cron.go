870ce17ec14eb95f5d791391c22a9630ee98051f
xieyp <20084040@qq.com>
2018-11-07 13:28:02 +0800

    添加定时任务

--- core/Cron.go
+++ core/Cron.go
@@ -3,16 +3,58 @@ package core
 import (
 	"goserver/utils"
 	"goserver/xtools/cron"
-)
+	"github.com/cihub/seelog"
+	"goserver/gpa"
+	"reflect"
 
-//cron := cron.New()
-//cron.AddFunc("0 * * * * ?", func() {
+	"goserver/xtools"
+)
 
 func (app *Module) Cron(ele *utils.Element) {
 	cron := cron.New()
-	cron.AddFunc(ele.MustAttr("Spec"), func() {
-
-	})
+	ns := ele.AllNodes()
+	for _, n := range ns {
+		job := &Job{job: n, dao: getGpa(n)}
+		spec, spb := n.AttrValue("Spec")
+		if spb {
+			cron.AddFunc(spec, job.Run)
+		} else {
+			job.Run()
+		}
+	}
 	cron.Start()
-	defer cron.Stop()
+}
+
+type Job struct {
+	job *utils.Element
+	dao *gpa.Gpa
+}
+
+func (job *Job) Run() {
+	inputs := make([]reflect.Value, 0)
+	m := reflect.ValueOf(job).MethodByName(job.job.Name())
+	if m.IsValid() {
+		m.Call(inputs)
+	} else {
+		seelog.Error("未知方法:" + job.job.Name())
+	}
+}
+
+func (job *Job) Sql() {
+	seelog.Info("~~Sql定时任务：", job.job.Value)
+	job.dao.Exec(job.job.Value)
+}
+func (job *Job) Http() {
+	getUrl, ext := job.job.AttrValue("GetUrl")
+	seelog.Info("~~Http定时任务：", job.job.Value)
+	if ext {
+		http := utils.Http{}
+		html, e := http.Get(getUrl)
+		if e == nil {
+			fd := &xtools.FmtData{Dao: job.dao}
+			fd.Spit(job.job.Node("Fmt"), string(html))
+		} else {
+			seelog.Error("HttpGet失败", getUrl)
+		}
+	}
 }
