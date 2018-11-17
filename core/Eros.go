2413c43c8016b064d77e03fc79c352a6267d5763
xieyp <20084040@qq.com>
2018-11-17 09:06:07 +0800

    EROS 的简单升级

--- core/Eros.go
+++ core/Eros.go
@@ -1,5 +1,11 @@
 package core
 
+/**
+
+<Eros AppDir="D:\apps\geek\app\platforms\android\WeexFrameworkWrapper\app\src\main\assets" Prefix="http://www.itgeek.top/appassets/">
+</Eros>
+
+ */
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ecdiy/goserver/utils"
@@ -11,84 +17,30 @@ import (
 	"github.com/ecdiy/goserver/webs"
 )
 
-var (
-	AppsDir    string
-	Prefix     string
-	AppVersion = make(map[string]string)
-	Apps       = []string{"mo"}
-)
-
 func (app *Module) Eros(ele *utils.Element) {
-	AppsDir = utils.EnvParam("Apps")
-	Prefix = utils.EnvParam("Prefix")
-	seelog.Info("Apps", Apps)
-
+	au := &AppUpdater{
+		Prefix:  ele.MustAttr("Prefix"),
+		AppsDir: ele.MustAttr("AppDir")}
+	au.initApp()
 	Web := getGin(ele)
-	Web.GET("/app/check", appCheck)
-	Web.GET("/app/reload", appReload)
-	initApp()
+	//"/app/check"
+	Web.GET(ele.Attr("CheckUrl", "/app/check"), au.Check)
+	Web.GET(ele.Attr("ReloadUrl", "/app/reload"), au.Reload)
+
 }
 
-func initApp() string {
-	res := ""
-	for _, app := range Apps {
-		initAppByName(app)
-	}
-	info := "\n\t\tAppsDir=" + AppsDir + ";Prefix=" + Prefix +
-		";AppVersion=" + fmt.Sprint(AppVersion) + ";\nReloadRes=" + res
-	seelog.Info(info)
-	return info
-}
-
-func initAppByName(app string) string {
-	file := AppsDir + "/" + app + "/bundle.config"
-	conf, e := ioutil.ReadFile(file)
-	if e == nil {
-		m := make(map[string]interface{})
-		e := json.Unmarshal(conf, &m)
-		if e == nil {
-			ver := fmt.Sprint(m["jsVersion"])
-
-			zipF := AppsDir + "/" + ver + ".zip"
-			_, e := os.Stat(zipF)
-			if os.IsNotExist(e) {
-				seelog.Info("!!not exist.", zipF)
-				bf, _ := ioutil.ReadFile(AppsDir + "/" + app + "/bundle.zip")
-				ioutil.WriteFile(zipF, bf, 0644)
-			} else {
-				seelog.Info("exist zip file.", zipF)
-			}
-
-			AppVersion[app] = ver
-			return file + ";\npath=" + Prefix + "/apps/" + ver + ".zip;\n"
-		} else {
-			seelog.Error("", e.Error())
-			return ""
-		}
-	} else {
-		return ",not exist=" + fmt.Sprint(e)
-	}
-}
-
-func appReload(c *gin.Context) {
-	c.String(200, initApp())
+type AppUpdater struct {
+	AppsDir, Prefix, LastVersion string
 }
 
 ///app/check?jsVersion=72f32bf37c3377e205d1e6c06b47645e&appName=molove&android=0.0.1&isDiff=1
-func appCheck(c *gin.Context) {
+func (au *AppUpdater) Check(c *gin.Context) {
 	wb := webs.NewParam(c)
 	jsVersion := wb.String("jsVersion")
-	appName := wb.String("appName")
 	android := wb.String("android")
 	if android != "" {
-		mv, mvb := AppVersion[appName]
-		if !mvb || len(AppVersion) == 0 {
-			seelog.Info("没有缓存,", appName)
-			initAppByName(appName)
-			mv, mvb = AppVersion[appName]
-		}
 		o := make(map[string]interface{})
-		if mvb && mv == jsVersion {
+		if au.LastVersion == jsVersion {
 			o["resCode"] = 4000
 			o["msg"] = "当前版本已是最新，不需要更新"
 		} else {
@@ -97,12 +49,45 @@ func appCheck(c *gin.Context) {
 			o["msg"] = "需要更新"
 			o["data"] = map[string]interface{}{
 				"diff":      false,
-				"jsVersion": mv,
-				"path":      Prefix + "/apps/" + mv + ".zip"}
+				"jsVersion": au.LastVersion,
+				"path":      au.Prefix + au.LastVersion + ".zip"}
 		}
 		c.JSON(200, o)
 		return
 	}
-	//TODO...
-	seelog.Info("appName=", appName)
+}
+
+func (au *AppUpdater) Reload(c *gin.Context) {
+	au.initApp()
+	c.String(200, au.LastVersion)
+}
+
+func (au *AppUpdater) initApp() {
+	au.LastVersion = ""
+	file := au.AppsDir + "/bundle.config"
+	conf, e := ioutil.ReadFile(file)
+	if e == nil {
+		m := make(map[string]interface{})
+		e := json.Unmarshal(conf, &m)
+		if e == nil {
+			ver := fmt.Sprint(m["jsVersion"])
+			zipF := au.AppsDir + "/" + ver + ".zip"
+			_, e := os.Stat(zipF)
+			if os.IsNotExist(e) {
+				seelog.Info("!!not exist.", zipF)
+				bf, _ := ioutil.ReadFile(au.AppsDir + "/bundle.zip")
+				ioutil.WriteFile(zipF, bf, 0644)
+			} else {
+				seelog.Info("exist zip file.", zipF)
+			}
+			au.LastVersion = ver
+			return
+		} else {
+			seelog.Error("", e.Error())
+			return
+		}
+	} else {
+		seelog.Error("文件错误：", file, e)
+		return
+	}
 }
